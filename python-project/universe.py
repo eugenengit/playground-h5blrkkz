f68af0dfa8fffe1a5e87d0716b144f455446234b
eugenengit <55707839+eugenengit@users.noreply.github.com>
2022-12-14 21:36:47 -0500

    Remove def count_...

--- python-project/universe.py
+++ python-project/universe.py
@@ -1,9 +1,2 @@
-'''
-def count_all_stars(galaxies):
-    total_stars = 0
-    for stars in galaxies:
-        total_stars = stars  # fix me!
-    return total_stars
-'''
 P = input('Entre la valeur du périmètre du rectangke en nètre!')
 print('la valeur entrée est ', P/100)
