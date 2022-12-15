b6d2d44712e381ffd6e717f7d26131f55e7562a0
eugenengit <55707839+eugenengit@users.noreply.github.com>
2022-12-14 21:35:19 -0500

    input perimetre

--- python-project/universe.py
+++ python-project/universe.py
@@ -1,5 +1,9 @@
+'''
 def count_all_stars(galaxies):
     total_stars = 0
     for stars in galaxies:
         total_stars = stars  # fix me!
     return total_stars
+'''
+P = input('Entre la valeur du périmètre du rectangke en nètre!')
+print('la valeur entrée est ', P/100)
